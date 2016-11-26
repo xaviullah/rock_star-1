50895de21cded59f01330e4ad4cc022ceba5c2fa
xaviullah <mdxavi20@gmail.com>
2016-11-26 18:16:00 +0600

    first commit

---
+++ js/abc.js
@@ -0,0 +1,10 @@
+/**
+ * Created by User1 on 11/23/2016.
+ */
+
+// credit by : fhjkfh
+Author url: www.xyz.com
+verson: 1.0
+copyright: fhjkfh
+//
+
---
+++ js/my.js
@@ -0,0 +1,11 @@
+/**
+ * Created by User1 on 11/23/2016.
+ */
+
+// credit by : xavi
+Author url: www.xyz.com
+verson: 1.0
+copyright: xavi
+//
+
+
